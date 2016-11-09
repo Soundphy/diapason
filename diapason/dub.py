2d2f0c335a25a009d8baebc9f5a017ef39cf6b15
Miguel Sánchez de León Peque <peque@neosit.es>
2016-11-09 21:41:38 +0100

    Add dub file for converting to other formats

---
+++ diapason/dub.py
@@ -0,0 +1,21 @@
+"""
+Dub module that can be used when ffmpeg is available to deal with different
+audio formats.
+"""
+from io import BytesIO
+
+from pydub import AudioSegment
+
+
+def convert_wav(wav, coding_format='mpeg', **kwargs):
+    """
+    Convert a WAV file to other formats.
+    """
+    assert coding_format in ('mpeg',)
+    if coding_format == 'mpeg':
+        coding_format = 'mp3'
+    bitrate = kwargs.get('bitrate', None)
+    converted = BytesIO()
+    audio = AudioSegment.from_wav(wav)
+    audio.export(converted, format=coding_format, bitrate=bitrate)
+    return converted
